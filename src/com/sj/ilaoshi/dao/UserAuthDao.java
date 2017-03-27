b46fa82a454cb006070e85182f1f9527d829194c
shiliting <861819089@qq.com>
2017-03-27 19:29:53 +0800

    拦截器

--- src/com/sj/ilaoshi/dao/UserAuthDao.java
+++ src/com/sj/ilaoshi/dao/UserAuthDao.java
@@ -1,8 +1,11 @@
 package com.sj.ilaoshi.dao;
 
+import com.sj.ilaoshi.entity.UserAuth;
+
 /**
  * 用户首选关联持久层
  * Created by slt on 2017/3/27.
  */
 public interface UserAuthDao {
+    UserAuth findOne(long id);
 }
---
+++ src/com/sj/ilaoshi/interceptor/LoginHandlerInterceptor.java
@@ -0,0 +1,32 @@
+package com.sj.ilaoshi.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 访问拦截器
+ * Created by slt on 2017/3/27.
+ */
+public class LoginHandlerInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse
+            , Object o) throws Exception {
+        //请求身份验证服务器，http请求
+
+
+        return true;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o
+            , ModelAndView modelAndView) throws Exception {
+
+
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o
+            , Exception e) throws Exception {
+
+    }
+}
